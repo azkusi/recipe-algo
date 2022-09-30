c9c45e8f66499997131333597a9d4475ec0c4472
azkusi <azkusi1@gmail.com>
2022-09-30 11:37:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port =  process.env.PORT || 5000;
+
+const cors = require('cors')
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
+
+app.use(cors());
+
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
