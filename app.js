50d4e5e9178fca4386f25a9b134b786d3841fbe2
azkusi <azkusi1@gmail.com>
2022-10-01 10:37:24 +0100

    export app variable from app.js

--- app.js
+++ app.js
@@ -18,3 +18,6 @@ app.use(express.static(path.resolve(__dirname, './client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
+
+
+module.exports = app;
