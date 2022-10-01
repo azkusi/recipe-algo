497880c1d0312477bb6bb5d51efdb3c53cc8b173
azkusi <azkusi1@gmail.com>
2022-10-01 10:24:48 +0100

    change of path for app.js express use

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.listen(port, () => {
 app.use(cors());
 
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
