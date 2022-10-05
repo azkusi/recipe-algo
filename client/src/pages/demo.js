b52c15a6c749177eb0d26890709a34c05a1ccf7f
azkusi <azkusi1@gmail.com>
2022-10-05 01:32:09 +0100

    build change

--- client/src/pages/demo.js
+++ client/src/pages/demo.js
@@ -30,18 +30,7 @@ import 'firebase/compat/firestore';
 import { React, useEffect, useState, useRef } from 'react';
 import { db } from '../config';
 import useBackgroundFunc from '../hooks/useBackgroundFunc';
-// import Timer from './timer';
 
-
-// firebase.initializeApp(config);
-// let db;
-// if (!firebase.apps.length) {
-//     firebase.initializeApp(config);
-//     db = firebase.firestore()
-
-//   }else {
-//     firebase.initializeApp(config);
-//   }
 
 function Demo() {
 
@@ -915,7 +904,8 @@ function Demo() {
                             <Row lg={2} md={2} style={{"border": "solid", "height": 0.4 * window.innerHeight}}>
                                 <Col style={{"border": "solid", "backgroundColor": background_colour_1}}>
                                     {recipe_1_upcoming.length > 0 ? <h2>{recipe_1_upcoming[0]["recipe-name"]}</h2> : <h2>Not in Use</h2>}
-                                    {(recipe_1_timer > 0) ? <h5>Waiting for cooking to finish in {recipe_1_timer} secs...</h5> : null}
+                                    {/* duration is in mins, recipe_x_timer is in seconds */}
+                                    {(recipe_1_timer > 0) ? <h5>Waiting for cooking to finish in {(recipe_1_timer / (60*60)) >= 1 ? (Math.floor(recipe_1_timer/60)) : ('00')}:{(59 >= (recipe_1_timer / 60) && (recipe_1_timer / 60)  >= 1) ? (recipe_1_timer / 60) : '00'}:{ 59 >= (recipe_1_timer) ? (recipe_1_timer) : '00'}</h5> : null}
                                     
                                     <img style={{"width": window.innerWidth * 0.25 * (window.innerHeight / window.innerWidth), "height": window.innerHeight * 0.25 }} src={Black_Circle} alt="black stove" />
                                 </Col >
